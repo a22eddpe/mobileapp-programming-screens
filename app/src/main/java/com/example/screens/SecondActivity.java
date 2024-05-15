64d776e215b0148f130366e68f0e7ef8b0ef024b
a22eddpe <129263688+a22eddpe@users.noreply.github.com>
2024-05-15 19:17:29 +0200

    Fixed placement

--- app/src/main/java/com/example/screens/SecondActivity.java
+++ app/src/main/java/com/example/screens/SecondActivity.java
@@ -3,7 +3,6 @@ package com.example.screens;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
 import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
